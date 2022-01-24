a95ccf0de526342f7dd56f38243d4e99cfd1298c
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-01-24 18:54:55 -0300

    add exercicio 2

---
+++ fundamentos/git-github-e-internet/exercicios/ex-2.sh
@@ -0,0 +1,66 @@
+#Exercicio 1
+
+cd Documents
+cd Trybe
+cd trybe-exercicios/
+cd fundamentos/
+cd git-github-e-internet/
+ls
+cd exercicios/
+ls
+
+#Exercicio 2
+
+git status
+git add .
+git commit -m "Initial commit"
+git status
+git push
+git status
+
+#Exercicio 3
+
+git branch trybe-skills-changes
+git branch 
+git checkout trybe-skills-changes 
+git branch 
+
+#Exercicio 4
+
+cat >> trybe-skills.txt 
+- HTML
+- CSS
+
+git add trybe-skills.txt 
+git commit -m "Alteracao no arquivo trybe-skills"
+git push -u origin trybe-skills-changes 
+
+#Exercicio 5 - Pull Request no GitHub
+
+#Exercicio 6
+
+git checkout master
+
+#Exercicio 7
+
+git branch
+
+#Exercicio 8
+
+git branch trybe-skills-updates
+git checkout trybe-skills-updates 
+git branch
+
+#Exercicio 9
+
+cat >> trybe-skills.txt 
+- React
+
+cat trybe-skills.txt 
+git add trybe-skills.txt 
+git commit -m "Alteracoes trybe-skills"
+git push -u origin trybe-skills-updates 
+git status
+
+#Exercicio 10 - Merge das branchs trybe-skills-changes e trybe-skills-updates com a main/master no GitHub
+
