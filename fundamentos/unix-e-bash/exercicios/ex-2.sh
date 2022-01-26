52616559aa705d392df96ebdee8e49c935b552fe
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-01-26 10:55:09 -0300

    adicao exercicio 2

---
+++ fundamentos/unix-e-bash/exercicios/ex-2.sh
@@ -0,0 +1,62 @@
+# Exercicio 1
+
+cd unix_tests
+curl -o countries.txt "https://gist.githubusercontent.com/kalinchernev/486393efcca01623b18d/raw/daa24c9fea66afb7d68f8d69f0c4b8eeb9406e83/countries"
+
+# Exercicio 2
+
+cat countries.txt 
+
+# Exercicio 3
+
+less countries.txt 
+
+# Exercicio 4
+
+less countries.txt
+/Zambia
+
+# Exercicio 5
+
+less countries.txt
+/Brazil
+
+# Exercicio 6
+
+less countries.txt
+-i
+/brazil
+
+# Exercicio 7
+
+grep -v "fox" phrases.txt 
+
+# Exercicio 8
+
+wc -w phrases.txt 
+
+# Exercicio 9
+
+wc -l phrases.txt 
+
+# Exercicio 10
+
+touch empty.tbt
+touch empty.pdf
+
+# Exercicio 11
+
+ls
+
+# Exercicio 12
+
+ls *.txt
+
+# Exercicio 13
+
+ls *.tbt *.txt
+
+# Exercicio 14
+
+man ls
+
