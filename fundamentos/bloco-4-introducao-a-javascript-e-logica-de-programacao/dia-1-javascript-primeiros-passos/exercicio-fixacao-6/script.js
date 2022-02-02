d3877fa0c2b9df7cd509fe6f11a314809b740bb0
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-02-02 16:35:53 -0300

    adicao ex fixacao 6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-fixacao-6/script.js
@@ -0,0 +1,15 @@
+let situacao = "aprovada";
+
+switch (situacao) {
+  case "aprovada":
+    console.log('Parabéns, você foi aprovada');
+    break;
+  case "lista":
+    console.log('Você está na lista de espera');
+    break;
+  case "reprovada":
+    console.log('Você foi reprovada');
+    break;
+  default:
+    console.log("Não se aplica");
+}
