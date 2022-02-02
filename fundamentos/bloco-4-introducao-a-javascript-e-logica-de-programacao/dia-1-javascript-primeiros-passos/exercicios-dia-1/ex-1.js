d1b361193ee00afa67b79e938ba338232f66013e
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-02-02 18:29:44 -0300

    adicao ex 1 e 2 do dia 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-dia-1/ex-1.js
@@ -0,0 +1,30 @@
+//----------- Exercicio 1 -----------
+
+let a = 3;
+let b = 4;
+let result;
+
+//----------- Exercicio 1.1 -----------
+
+result = a + b;
+console.log(result);
+
+//----------- Exercicio 1.2 -----------
+
+result = a - b;
+console.log(result);
+
+//----------- Exercicio 1.3 -----------
+
+result = a * b;
+console.log(result);
+
+//----------- Exercicio 1.4 -----------
+
+result = a / b;
+console.log(result);
+
+//----------- Exercicio 1.5 -----------
+
+result = a % b;
+console.log(result);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-dia-1/ex-2.js
@@ -0,0 +1,10 @@
+//----------- Exercicio 2 -----------
+
+let x = 10;
+let y = 7;
+
+if (x > y) {
+  console.log("O maior número é: ", x);
+} else {
+  console.log("O maior número é ", y);
+}
