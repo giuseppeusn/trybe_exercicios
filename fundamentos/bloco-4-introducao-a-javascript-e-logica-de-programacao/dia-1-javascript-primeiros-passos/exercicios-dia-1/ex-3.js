ff1c4bc0a3cdfb76ec72b89063ddc840c747c574
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-02-02 18:32:51 -0300

    adicao ex 3 do dia 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-dia-1/ex-3.js
@@ -0,0 +1,13 @@
+//----------- Exercicio 3 -----------
+
+let a = 30;
+let b = 25;
+let c = 20;
+
+if (a > b && a > c) {
+  console.log("O maior número é: ", a);
+} else if (b > a && b > c) {
+  console.log("O maior número é: ", b);
+} else {
+  console.log("O maior número é: ", c);
+}
