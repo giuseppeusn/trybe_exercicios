21d135f7b81f50de2c82d8044c2e599d059d7a67
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2022-02-03 15:53:14 -0300

    adicionando ex fixacao restantes

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-fixacao/ex-fix-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-fixacao/ex-fix-1.js
@@ -1,4 +1,9 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu.indexOf('Serviços');
+let indexOfPortfolio = menu.indexOf('Portfólio');
 
+menu.push('Contato');
+
+console.log(menu);
+console.log(indexOfPortfolio);
 console.log(menuServices);
