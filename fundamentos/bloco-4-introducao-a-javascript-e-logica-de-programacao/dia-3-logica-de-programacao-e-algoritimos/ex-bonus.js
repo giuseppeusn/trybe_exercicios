0bee9ea0240c5a6da708409431c5d318dfd89cd7
Giuseppe <giuseppeuhlmann@gmail.com>
2022-02-06 13:14:16 -0300

    exercicio bonus finalizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritimos/ex-bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritimos/ex-bonus.js
@@ -7,7 +7,7 @@ for (let y = 0; y < n; y += 1) {
   for (let x = 0; x < n; x += 1) {
     linha[x] = '*';
   }
-  // console.log(linha.join(""));
+  console.log(linha.join(""));
 }
 
 // ----------- Exercicio 2 -----------
@@ -20,7 +20,7 @@ for (let y = 0; y < n; y += 1) {
     linha[x] = '*';
   }
   z -= 1;
-  // console.log(linha.join(""));
+  console.log(linha.join(""));
 }
 
 // ----------- Exercicio 3 -----------
@@ -36,7 +36,7 @@ for (let y = 0; y < n; y += 1) {
     }    
     b += 1;
   }
-  // console.log(linha.join(""));
+  console.log(linha.join(""));
 }
 
 // ----------- Exercicio 4 -----------
@@ -56,13 +56,44 @@ for (let y = 0; y < (n/2 + .5); y += 1) {
     b -= 1;
   }
   elem += 1;
-  // console.log(linha.join(""));
+  console.log(linha.join(""));
 }
 
 // ----------- Exercicio 5 -----------
 
+let meio = (n + 1) / 2;
+let esq = meio;
+let dir = meio;
+
 if (n % 2 == 1) {
+  for (let y = 1; y <= meio; y += 1) {
+    let linha = [];
+    for (let x = 1; x <= n; x += 1) {  
+      if (x == esq || x == dir || y == meio) {
+        linha[x] = '*';
+      } else {
+        linha[x] = ' ';
+      }
+    }
+    esq -= 1;
+    dir += 1;
+    console.log(linha.join(""));
+  }
 } else {
   console.log('Número deve ser ímpar');
 }
 
+// ----------- Exercicio 6 -----------
+
+let num = 97;
+let primo = true;
+
+for (let x = 0; x <= num; x += 1) {
+  for (let y = 2; y < x; y += 1) {
+    if (num % y === 0) {
+      primo = false;
+    } 
+  }
+}
+
+primo == true ? console.log(num + ' é primo') : console.log(num + ' não é primo');
