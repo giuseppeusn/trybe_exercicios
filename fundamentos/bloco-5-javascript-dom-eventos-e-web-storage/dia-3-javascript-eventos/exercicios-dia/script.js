d7ea5a6bc6992aca371e3b5e5281c21e8c69cd57
Giuseppe <giuseppeuhlmann@gmail.com>
2022-02-12 11:51:03 -0300

    adicao ex 1 do dia

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicios-dia/script.js
@@ -0,0 +1,38 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+function createDays() {
+  let days = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let daysList = document.getElementById('days');
+
+  for (x in days) {
+    let li = document.createElement('li');
+    daysList.appendChild(li);
+    li.className = 'day';
+    
+    if (days[x] == 24 || days[x] == 25 || days[x] == 31) {
+      li.classList.add('holiday');
+    } 
+    
+    if (days[x] == 4 || days[x] == 11 || days[x] == 18 || days[x] == 25 ) {
+      li.classList.add('friday');
+    } 
+
+    li.innerText = days[x];
+  }
+}
+
+createDays();
+
