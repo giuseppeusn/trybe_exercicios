53b6d87a0773b9352894a84b631aad349c1531d9
Giuseppe <giuseppeuhlmann@gmail.com>
2022-02-15 12:47:24 -0300

    adicionando alteracoes ao ex

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios-dia/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios-dia/script.js
@@ -3,16 +3,20 @@ let txtPick = document.getElementById('txtPicker');
 let font = document.getElementById('font');
 let fontSize = document.getElementById('fontSize');
 let spacing = document.getElementById('spacing');
+let cfg = document.getElementById('config');
+let menu = document.querySelector('table');
 
 
 let body = document.querySelector('body');
-let paragraph = document.querySelectorAll('p');
+let title = document.querySelector('h2');
+let paragraph = document.querySelectorAll('section p');
 
 bgPick.onchange = changeBgColor;
 txtPick.onchange = changeTxtColor;
 font.onchange = changeFont;
 fontSize.onchange = changeFontSize;
 spacing.onchange = changeSpacing;
+cfg.onclick = openMenu;
 
 initialization();
 
@@ -25,9 +29,8 @@ function changeBgColor(event){
 function changeTxtColor(event) {
   let color = event.target.value;
 
-  for (let x = 0; x < paragraph.length; x += 1) {
-    paragraph[x].style.color = color;
-  }
+  paragraph[0].style.color = color;
+  title.style.color = color;
   
   localStorage.setItem('txt-color',color);
 }
@@ -35,22 +38,35 @@ function changeTxtColor(event) {
 function changeFontSize(event) {
   let size = event.target.value;
 
-  paragraph[paragraph.length - 1].style.fontSize = size + 'px';
+  paragraph[0].style.fontSize = size + 'px';
   localStorage.setItem('fontSize',size);
 }
 
 function changeSpacing(event) {
   let lineSpacing = event.target.value;
 
-  paragraph[paragraph.length - 1].style.lineHeight = lineSpacing + 'px';
+  paragraph[0].style.lineHeight = lineSpacing + 'px';
   localStorage.setItem('lineSpacing',lineSpacing);
 }
 
 function changeFont(event) {
   let fontFamily = event.target.value;
 
-  paragraph[paragraph.length - 1].style.fontFamily = fontFamily;
+  title.style.fontFamily = fontFamily;
+  paragraph[0].style.fontFamily = fontFamily;
   localStorage.setItem('font',fontFamily);
+}
+
+function openMenu() {
+  let visi = menu.style.opacity;
+  console.log(visi);
+
+  if(visi == '0') {
+    menu.style.opacity = '1';
+  } else {
+    menu.style.opacity = '0';
+  }
+  
 }
 
 function initialization() {
@@ -66,13 +82,12 @@ function initialization() {
   fontSize.value = size;
   spacing.value = lineHeight;
 
-  for (let x = 0; x < paragraph.length; x += 1) {
-    paragraph[x].style.color = colorTxt;
-  }
-
+  paragraph[0].style.color = colorTxt;
+  title.style.color = colorTxt;
   body.style.backgroundColor = colorBg;
-  paragraph[paragraph.length - 1].style.fontFamily = fontFamily;
-  paragraph[paragraph.length - 1].style.fontSize = size + 'px';
-  paragraph[paragraph.length - 1].style.lineHeight = lineHeight + 'px';
+  paragraph[0].style.fontFamily = fontFamily;
+  title.style.fontFamily = fontFamily;
+  paragraph[0].style.fontSize = size + 'px';
+  paragraph[0].style.lineHeight = lineHeight + 'px';
 
 }
