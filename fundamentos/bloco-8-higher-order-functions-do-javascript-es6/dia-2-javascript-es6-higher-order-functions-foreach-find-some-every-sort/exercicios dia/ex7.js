3935dcd5a7b0768a2dcde25e47034c1fdfb70a41
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-12 20:48:59 -0300

    adicionando nova solucao

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios dia/ex7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios dia/ex7.js
@@ -1,6 +1,8 @@
 const object = require('./object.js');
 const books = object();
 
+//Solução 1
+
 const authorUnique = () => {
   let birthYearArray = [];
   let answer = true;
@@ -19,4 +21,24 @@ const authorUnique = () => {
 
   return answer;
 }
+
 console.log(authorUnique());
+
+//Solução 2 (filter)
+
+const authorUnique2 = () => {
+  let birthYearArray = [];
+  let answer = true;
+
+  books.forEach((item) => birthYearArray.push(item.author.birthYear));
+
+  if(findDuplicates(birthYearArray).length !== 0) {
+    answer = false;
+  }
+
+  return answer;
+}
+
+let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+
+console.log(authorUnique2());
