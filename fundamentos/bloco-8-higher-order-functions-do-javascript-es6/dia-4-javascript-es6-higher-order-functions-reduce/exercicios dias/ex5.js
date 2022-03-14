93b6102d40e81f6994d9d121bc57d6a8af83bea4
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-14 15:44:08 -0300

    adicionando ex 5 do dia

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios dias/ex5.js
@@ -0,0 +1,12 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, name) => acc + name.match(/a/gi).length, 0);
+}
+
+console.log(containsA());
+
