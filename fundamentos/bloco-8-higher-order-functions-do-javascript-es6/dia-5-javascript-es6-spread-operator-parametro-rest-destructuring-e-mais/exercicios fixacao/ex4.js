68e4daa0a8f2fe31b8dd72da3cadd986de8a6aaa
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-15 15:34:17 -0300

    adicionando ex 4 de fixacao

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios fixacao/ex4.js
@@ -0,0 +1,9 @@
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida);
+
+[ animal, bebida, comida ] = [ comida, animal, bebida ];
+
+console.log(comida, animal, bebida);
