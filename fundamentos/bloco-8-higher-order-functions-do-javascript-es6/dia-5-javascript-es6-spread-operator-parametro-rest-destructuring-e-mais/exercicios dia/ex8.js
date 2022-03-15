226c5b35b1282b80af05ead0fadc85055b8ef29d
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-15 19:07:30 -0300

    adicionando ex 8 do dia

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios dia/ex8.js
@@ -0,0 +1,7 @@
+const greet = (name, greeting = 'Hi') => {
+  return `${greeting} ${name}`
+}
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
