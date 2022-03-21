c911e44165477d5e4aa9159c5a645f9a775b0d1f
Giuseppe <giuseppeuhlmann@gmail.com>
2022-03-21 19:37:35 -0300

    adicionando ex 6 do dia

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios dia/ex6.test.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios dia/ex6.test.js
@@ -1,19 +1,26 @@
-const { expect } = require('@jest/globals');
 const { getPokemonDetails } = require('./ex6');
 
 describe("A função getPokemonDetails", () => {
   it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
-    const func =  (pokemon) => pokemon.name === 'outro';
-    
+    const func = (pokemon) => pokemon.name === 'outro';
+
     function callback(error) {
-      expect(error).toEqual('Não temos esse pokémon para você :(');
+      expect(error).toEqual( new Error('Não temos esse pokémon para você :('));
       done();
     }
 
     getPokemonDetails(func, callback);
   });
 
-  it("retorna um pokemon que existe no banco de dados", () => {
-    // Escreva aqui seu código
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const string = 'Olá, seu pokémon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember';
+    const func = (pokemon) => pokemon.name === 'Charmander';
+
+    function callback(err, result) {
+      expect(result).toBe(string);
+      done();
+    }
+
+    getPokemonDetails(func, callback);
   });
 });
