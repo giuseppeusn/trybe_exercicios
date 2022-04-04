7b5be3dce6c044c32a9eb900f229f84d91fc0576
Giuseppe <giuseppeuhlmann@gmail.com>
2022-04-04 16:00:24 -0300

    feat: adicionando ex de fixacao

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios fixacao/hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class perfil extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Giuseppe Nunes</h1>
+        <p>Estudante de desenvolvimento web na Trybe</p>
+      </div>
+    )
+  }
+}
+
+export default perfil;
