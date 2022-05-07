104b9bc2e014aea6fabefac7c783bbfb3479c833
Giuseppe <giuseppeuhlmann@gmail.com>
2022-05-07 14:17:29 -0300

    feat: adicionando ex 5 do dia

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/ex1/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/ex1/service.test.js
@@ -1,69 +1,92 @@
-const randomNumber = () => Math.floor(Math.random() * 100);
+const service = require('./service');
 
 describe('Testes exercícios 1, 2 e 3', () => {
   afterEach(() => jest.clearAllMocks());
 
   it('Verifica se o retorno da função "randomNumber" está correto', () => {
-    const randomNumber = jest.fn().mockReturnValue(10);
+    service.randomNumber = jest.fn().mockReturnValue(10);
 
-    expect(randomNumber()).toBe(10);
-    expect(randomNumber).toHaveBeenCalled()
-    expect(randomNumber).toHaveBeenCalledTimes(1)
+    expect(service.randomNumber()).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled()
+    expect(service.randomNumber).toHaveBeenCalledTimes(1)
   });
 
   it('Verifica se o retorno da função "randomNumber" mockada (divisão) está correto', () => {
-    const randomNumber = jest.fn().mockImplementation((a, b) => a / b);
+    service.randomNumber = jest.fn().mockImplementation((a, b) => a / b);
 
-    expect(randomNumber(10, 2)).toBe(5);
-    expect(randomNumber).toHaveBeenCalled();
-    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber(10, 2)).toBe(5);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
   });
 
   it('Verifica se o retorno da função "randomNumber" mockada (multiplicação) está correto', () => {
-    let randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
 
-    expect(randomNumber(10, 2, 1)).toBe(20);
-    expect(randomNumber).toHaveBeenCalled();
-    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber(10, 2, 1)).toBe(20);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
 
-    randomNumber.mockReset();
-    randomNumber = jest.fn().mockImplementation((a) => a * 2);
+    service.randomNumber.mockReset();
+    service.randomNumber = jest.fn().mockImplementation((a) => a * 2);
 
-    expect(randomNumber(5)).toBe(10);
-    expect(randomNumber).toHaveBeenCalled();
-    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber(5)).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
   });
 })
 
-const toUpper = (string) => string.toUpperCase();
-const firstLetter = (string) => string.split('')[0];
-const oneString = (str1, str2) => str1.concat(str2);
+
 
 describe('Testes exercício 4', () => {
   it('Verifica se o retorno da função "toUpper" mockada (toLower) está correto', () => {
-    const toUpper = jest.fn().mockImplementation((string) => string.toLowerCase());
+    service.toUpper = jest.fn().mockImplementation((string) => string.toLowerCase());
 
-    expect(toUpper('TESTE')).toBe('teste');
-    expect(toUpper('TeStE')).toBe('teste');
-    expect(toUpper).toHaveBeenCalled();
-    expect(toUpper).toHaveBeenCalledTimes(2);
+    expect(service.toUpper('TESTE')).toBe('teste');
+    expect(service.toUpper('TeStE')).toBe('teste');
+    expect(service.toUpper).toHaveBeenCalled();
+    expect(service.toUpper).toHaveBeenCalledTimes(2);
   })
 
   it('Verifica se o retorno da função "firstLetter" mockada (última letra) está correto', () => {
-    const firstLetter = jest.fn().mockImplementation((string) => string.split('')[string.length - 1]);
+    service.firstLetter = jest.fn().mockImplementation((string) => string.split('')[string.length - 1]);
 
-    expect(firstLetter('teste')).toBe('e');
-    expect(firstLetter('teste')).not.toBe('t');
-    expect(firstLetter).toHaveBeenCalled();
-    expect(firstLetter).toHaveBeenCalledTimes(2);
+    expect(service.firstLetter('teste')).toBe('e');
+    expect(service.firstLetter('teste')).not.toBe('t');
+    expect(service.firstLetter).toHaveBeenCalled();
+    expect(service.firstLetter).toHaveBeenCalledTimes(2);
   })
 
   it('Verifica se o retorno da função "oneString" mockada (3 params) está correto', () => {
-    const oneString = jest.fn().mockImplementation((str1, str2, str3) => str1.concat(str2, str3));
+    service.oneString = jest.fn().mockImplementation((str1, str2, str3) => str1.concat(str2, str3));
 
-    expect(oneString('teste1','teste2','teste3')).toBe('teste1teste2teste3');
-    expect(oneString('teste1','teste2')).not.toBe('teste1teste2');
-    expect(oneString).toHaveBeenCalled();
-    expect(oneString).toHaveBeenCalledTimes(2);
+    expect(service.oneString('teste1','teste2','teste3')).toBe('teste1teste2teste3');
+    expect(service.oneString('teste1','teste2')).not.toBe('teste1teste2');
+    expect(service.oneString).toHaveBeenCalled();
+    expect(service.oneString).toHaveBeenCalledTimes(2);
+  })
+})
+
+describe('Teste exercício 5', () => {
+  service.dogApi = jest.fn();
+  afterEach(() => service.dogApi.mockReset());
+
+  it('Verifica se o retorno da API está correto', async () => {
+    service.dogApi.mockResolvedValue('request sucess');
+    service.dogApi();
+
+    expect(service.dogApi).toHaveBeenCalled();
+    expect(service.dogApi).toHaveBeenCalledTimes(1);
+    await expect(service.dogApi()).resolves.toBe('request sucess');
+    expect(service.dogApi).toHaveBeenCalledTimes(2);
+  })
+
+  it('Verifica se o retorno da API falha', async () => {
+    service.dogApi.mockResolvedValue('request failure');
+    service.dogApi();
+
+    expect(service.dogApi).toHaveBeenCalled();
+    expect(service.dogApi).toHaveBeenCalledTimes(1);
+    await expect(service.dogApi()).resolves.toBe('request failure');
+    expect(service.dogApi).toHaveBeenCalledTimes(2);
   })
 })
