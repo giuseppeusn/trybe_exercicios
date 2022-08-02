6c17885f1f563a5441d04d1d093345cb29a065c0
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-02 15:54:20 -0300

    fix: undefined parameter returns false

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-6-dinamica-express/validateData.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-6-dinamica-express/validateData.js
@@ -1,16 +1,18 @@
 const validateUsername = (username) => {
-  if (username.length > 3) return true;
+  if (username && username.length > 3) return true;
 
   return false;
 }
 
 const validateEmail = (email) => {
+  if (!email) return false;
+
   const regex = /\S+@\S+\.\S+/;
   return regex.test(email);
 }
 
 const validatePassword = (password) => {
-  if (password.length >= 4 && password.length <= 8) return true;
+  if (password && password.length >= 4 && password.length <= 8) return true;
 
   return false;
 }
