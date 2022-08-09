8f140e83766353d7384646f761eac76585780910
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-08 21:20:33 -0300

    fix: small issues

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/cepController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/cepController.js
@@ -2,11 +2,11 @@ const cepServices = require('../services/cepServices');
 
 const getCep = async (req, res) => {
   const { cep } = req.params;
-  const { reqCode, code, message, response } = await cepServices.getCep(cep);
+  const { resCode, code, message, response } = await cepServices.getCep(cep);
 
-  if (message) res.status(reqCode).json({ error: { code, message } });
+  if (message) res.status(resCode).json({ error: { code, message } });
 
-  res.status(reqCode).json(response);
+  res.status(resCode).json(response);
 }
 
 module.exports = { getCep };
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepServices.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepServices.js
@@ -4,17 +4,17 @@ const getCep = async (cep) => {
   const cepRegex = /^\d{5}-?\d{3}$/;
 
   if (!cepRegex.test(cep)) {
-    return { reqCode: 400, code: 'invalidData', message: 'CEP inválido' };
+    return { resCode: 400, code: 'invalidData', message: 'CEP inválido' };
   }
 
   const result = await cepModels.getCep(cep);
   result[0].cep = `${result[0].cep.substring(0, 5)}-${result[0].cep.substring(5, 8)}`;
 
   if (!result.length) {
-    return { reqCode: 404, code: 'notFound', message: 'CEP não encontrado' };
+    return { resCode: 404, code: 'notFound', message: 'CEP não encontrado' };
   }
 
-  return { reqCode: 200, response: result };
+  return { resCode: 200, response: result[0] };
 }
 
 module.exports = { getCep };
